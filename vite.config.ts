4e4756ed0d474dc5f6f5ce513c0b99e4bf93d1bc
onegamerstory <gamecuts0412@gmail.com>
2025-11-29 15:41:00 +0300

    resources for v1.0.0 and prepare for release build

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,30 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+    // Загрузка переменных окружения
+    const env = loadEnv(mode, '.', '');
+    
+    return {
+        
+        // Генерирует относительные пути к ресурсам (./assets/...)
+        base: './', 
+        
+        server: {
+            port: 3000,
+            host: '0.0.0.0',
+        },
+        plugins: [react()],
+        
+        // Определение переменных окружения для Frontend (если нужно)
+        define: {
+            'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        },
+        
+        resolve: {
+            alias: {
+                // Предполагая, что исходный код находится в 'src'
+                '@': path.resolve(__dirname, 'src'), 
+            }
+        }
+    };
 });
